dcc599ee963c4adc0e30b07d3ed5e273fc1cb5dc
Leonid Savtchenko <LeonidSavtchenko@users.noreply.github.com>
2019-09-16 10:54:30 +0100

    change input parameters

--- host/Core/scripts/win-lin/params.bat
+++ host/Core/scripts/win-lin/params.bat
@@ -2,8 +2,8 @@
 
 rem The next parameters must be adjusted before the first launch
 set HEADNODEIP=144.82.46.83
-set LOGIN=savtchenko
-set PASSWORD=h1pp0c
+set LOGIN=*****
+set PASSWORD=*****
 set HEADNODEWORKERDIR=/home/savtchenko/amcbridge/phase14/nano2/clusterCaSim/hpc
 
 rem Also, you may need to adjust "GSDIR" path in %HEADNODEWORKERDIR%/build/lin_release.sh

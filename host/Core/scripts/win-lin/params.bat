ef5e107c138277a32f76d20d8dac8bd719bc4413
Leonid Savtchenko <LeonidSavtchenko@users.noreply.github.com>
2019-09-17 09:14:38 +0100

    Param.bat modification

--- host/Core/scripts/win-lin/params.bat
+++ host/Core/scripts/win-lin/params.bat
@@ -1,10 +1,16 @@
 @echo off
 
 rem The next parameters must be adjusted before the first launch
-set HEADNODEIP=144.82.46.83
-set LOGIN=savtchenko
-set PASSWORD=h1pp0c
-set HEADNODEWORKERDIR=/home/savtchenko/amcbridge/phase14/nano2/clusterCaSim/hpc
+REM set HEADNODEIP=144.82.46.83
+REM set LOGIN=****
+REM set PASSWORD=****
+REM set HEADNODEWORKERDIR=/home/savtchenko/amcbridge/phase14/nano2/clusterCaSim/hpc
+
+
+set HEADNODEIP=34.244.226.81
+set LOGIN=ucl
+set PASSWORD=fff -agent -P 2222
+set HEADNODEWORKERDIR=/ucl/AstroNeuron-master/hpc
 
 rem Also, you may need to adjust "GSDIR" path in %HEADNODEWORKERDIR%/build/lin_release.sh
 
